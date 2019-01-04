745e764435ede4c17d2b75eb625d969eb79d42af
Piotr Kocia <p.kocia@wp.pl>
2019-01-04 18:30:17 +0100

    Fix fix

--- engine/source/engine/private/mesh/plane.cpp
+++ engine/source/engine/private/mesh/plane.cpp
@@ -1,10 +1,10 @@
 #include "mesh/plane.hpp"
 
 Plane::Plane()
-    : Mesh({Vertex(Vector3(-1.0f, 1.0f, 0.0f), Vector3(0.0f, 1.0f, 0.0f), Vector2(0.0f, 1.0f)),
-            Vertex(Vector3(-1.0f, -1.0f, 0.0f), Vector3(0.0f, 1.0f, 0.0f), Vector2(0.0f, 0.0f)),
-            Vertex(Vector3(1.0f, -1.0f, 0.0f), Vector3(0.0f, 1.0f, 0.0f), Vector2(1.0f, 0.0f)),
-            Vertex(Vector3(-1.0f, 1.0f, 0.0f), Vector3(0.0f, 1.0f, 0.0f), Vector2(0.0f, 1.0f)),
-            Vertex(Vector3(1.0f, -1.0f, 0.0f), Vector3(0.0f, 1.0f, 0.0f), Vector2(1.0f, 0.0f)),
-            Vertex(Vector3(1.0f, 1.0f, 0.0f), Vector3(0.0f, 1.0f, 0.0f), Vector2(1.0f, 1.0f))},
+    : Mesh({Vertex(Vector3(-1.0f, 1.0f, 0.0f), Vector3(0.0f, 0.0f, 1.0f), Vector2(0.0f, 1.0f)),
+            Vertex(Vector3(-1.0f, -1.0f, 0.0f), Vector3(0.0f, 0.0f, 1.0f), Vector2(0.0f, 0.0f)),
+            Vertex(Vector3(1.0f, -1.0f, 0.0f), Vector3(0.0f, 0.0f, 1.0f), Vector2(1.0f, 0.0f)),
+            Vertex(Vector3(-1.0f, 1.0f, 0.0f), Vector3(0.0f, 0.0f, 1.0f), Vector2(0.0f, 1.0f)),
+            Vertex(Vector3(1.0f, -1.0f, 0.0f), Vector3(0.0f, 0.0f, 1.0f), Vector2(1.0f, 0.0f)),
+            Vertex(Vector3(1.0f, 1.0f, 0.0f), Vector3(0.0f, 0.0f, 1.0f), Vector2(1.0f, 1.0f))},
            {0, 1, 2, 3, 4, 5}, {}) {}
