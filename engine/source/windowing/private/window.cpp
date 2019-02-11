f3bf000e02b5838eaa5ebdc3dbd9b9e84ec1726a
Piotr Kocia <p.kocia@wp.pl>
2019-02-11 13:18:28 +0100

    Fixed-ish repeat, temporarily added debug logging

--- engine/source/windowing/private/window.cpp
+++ engine/source/windowing/private/window.cpp
@@ -18,7 +18,7 @@ void mouse_position_callback(GLFWwindow*, double param_x, double param_y);
 void scroll_callback(GLFWwindow*, double offset_x, double offset_y);
 void keyboard_callback(GLFWwindow*, int, int, int, int);
 
-Window::Window(uint32_t width, uint32_t height): window_width(width), window_height(height) {
+Window::Window(uint32_t width, uint32_t height) : window_width(width), window_height(height) {
     GE_assert(window_width > 0 && window_height > 0, "Window dimensions may not be 0");
 
     glfwInit();
@@ -103,6 +103,9 @@ void mouse_button_callback(GLFWwindow*, int button, int action, int mods) {
     input_manager.input_event_queue.emplace_back(key, value);
 }
 
+#include <iostream>
+#include <string>
+
 void mouse_position_callback(GLFWwindow*, double param_x, double param_y) {
     static float last_x = Engine::get_window().height() / 2.0f;
     static float last_y = Engine::get_window().width() / 2.0f;
@@ -114,6 +117,8 @@ void mouse_position_callback(GLFWwindow*, double param_x, double param_y) {
     last_x = x;
     last_y = y;
 
+    //std::cout << "Mouse x: " << offset_x << "; Mouse y: " << offset_y << "\n";
+
     Input_Manager& input_manager = Engine::get_input_manager();
     input_manager.input_event_queue.emplace_back(Key::mouse_x, offset_x);
     input_manager.input_event_queue.emplace_back(Key::mouse_y, offset_y);
@@ -124,7 +129,7 @@ void scroll_callback(GLFWwindow*, double offset_x, double offset_y) {
     input_manager.input_event_queue.emplace_back(Key::mouse_scroll, static_cast<float>(offset_y));
 }
 
-void keyboard_callback(GLFWwindow*, int key, int scancode, int action, int mods) {
+void keyboard_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
     // clang-format off
     static std::unordered_map<int32_t, Key> keyboard_button_map({
         {GLFW_KEY_A, Key::a},
@@ -153,12 +158,52 @@ void keyboard_callback(GLFWwindow*, int key, int scancode, int action, int mods)
         {GLFW_KEY_X, Key::x},
         {GLFW_KEY_Y, Key::y},
         {GLFW_KEY_Z, Key::z},
-        {GLFW_KEY_ESCAPE, Key::escape},
+        {GLFW_KEY_ESCAPE, Key::escape}
+    });
+
+    static std::unordered_map<int32_t, std::string> keyboard_string_map({
+        {GLFW_KEY_A, "Key::a"},
+        {GLFW_KEY_B, "Key::b"},
+        {GLFW_KEY_C, "Key::c"},
+        {GLFW_KEY_D, "Key::d"},
+        {GLFW_KEY_E, "Key::e"},
+        {GLFW_KEY_F, "Key::f"},
+        {GLFW_KEY_G, "Key::g"},
+        {GLFW_KEY_H, "Key::h"},
+        {GLFW_KEY_I, "Key::i"},
+        {GLFW_KEY_J, "Key::j"},
+        {GLFW_KEY_K, "Key::k"},
+        {GLFW_KEY_L, "Key::l"},
+        {GLFW_KEY_M, "Key::m"},
+        {GLFW_KEY_N, "Key::n"},
+        {GLFW_KEY_O, "Key::o"},
+        {GLFW_KEY_P, "Key::p"},
+        {GLFW_KEY_Q, "Key::q"},
+        {GLFW_KEY_R, "Key::r"},
+        {GLFW_KEY_S, "Key::s"},
+        {GLFW_KEY_T, "Key::t"},
+        {GLFW_KEY_U, "Key::u"},
+        {GLFW_KEY_V, "Key::v"},
+        {GLFW_KEY_W, "Key::w"},
+        {GLFW_KEY_X, "Key::x"},
+        {GLFW_KEY_Y, "Key::y"},
+        {GLFW_KEY_Z, "Key::z"},
+        {GLFW_KEY_ESCAPE, "Key::escape"}
     });
     // clang-format on
 
+    //std::string str = keyboard_string_map.at(key);
+    //std::cout << action << " " << str << "\n";
+
     Key mapped_key = keyboard_button_map.at(key);
-    float value = static_cast<float>(action); // GLFW_PRESS is 1, GLFW_RELEASE is  0
-    Input_Manager& input_manager = Engine::get_input_manager();
-    input_manager.input_event_queue.emplace_back(mapped_key, value);
+
+	// Temporary for debugging
+    if (mapped_key == Key::escape)
+        glfwSetWindowShouldClose(window, true);
+
+    if (action == GLFW_PRESS || action == GLFW_RELEASE) {
+		float value = static_cast<float>(action); // GLFW_PRESS is 1, GLFW_RELEASE is  0
+		Input_Manager& input_manager = Engine::get_input_manager();
+		input_manager.input_event_queue.emplace_back(mapped_key, value);
+    }
 }
