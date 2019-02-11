ae689006f61692e70be7735e2113a4e12bd1e2de
Piotr Kocia <p.kocia@wp.pl>
2019-02-12 00:14:42 +0100

    max, min, abs, step_to_value math functions

--- engine/source/core/private/math/math.cpp
+++ engine/source/core/private/math/math.cpp
@@ -16,4 +16,25 @@ namespace math {
             return 0.0f;
         }
     }
+
+    float max(float a, float b) {
+        return a > b ? a : b;
+    }
+
+    float min(float a, float b) {
+        return a < b ? a : b;
+    }
+
+	float abs(float a) {
+        return a >= 0 ? a : -a;
+	}
+
+    float step_to_value(float current, float target, float change) {
+        float delta = target - current;
+        if (abs(delta) > change) {
+            return current + sign(delta) * change;
+		} else {
+            return target;
+		}
+    }
 } // namespace math
