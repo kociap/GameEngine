8828091155cc9121f1aa3046ec66b7543000c71e
Piotr Kocia <p.kocia@wp.pl>
2019-02-11 13:04:59 +0100

    Predefined colors

--- engine/source/core/private/color.cpp
+++ engine/source/core/private/color.cpp
@@ -1,13 +1,19 @@
 #include "color.hpp"
 
+Color const Color::white = Color(1.0f, 1.0f, 1.0f);
+Color const Color::black = Color(0.0f, 0.0f, 0.0f);
+Color const Color::red = Color(1.0f, 0.0f, 0.0f);
+Color const Color::green = Color(0.0f, 1.0f, 0.0f);
+Color const Color::blue = Color(0.0f, 0.0f, 1.0f);
+
 Color::Color() {}
-Color::Color(float red, float green, float blue): r(red), g(green), b(blue) {}
+Color::Color(float red, float green, float blue) : r(red), g(green), b(blue) {}
 
 Color& Color::operator*=(Color const& c) {
     r *= c.r;
     g *= c.g;
     b *= c.b;
-	return *this;
+    return *this;
 }
 
 Color operator*(Color const& c1, Color const& c2) {
