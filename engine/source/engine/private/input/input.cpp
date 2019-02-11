02bdf19fa3d71da241c10f98ebdae1c8324e605b
Piotr Kocia <p.kocia@wp.pl>
2019-02-11 13:19:13 +0100

    Changed get_axis to use get_axis_raw because no implementation yet

--- engine/source/engine/private/input/input.cpp
+++ engine/source/engine/private/input/input.cpp
@@ -4,13 +4,14 @@
 #include "input/input_core.hpp"
 
 float input::get_axis(Input_Axis axis_name) {
-    Input_Manager& input_manager = Engine::get_input_manager();
+    /*Input_Manager& input_manager = Engine::get_input_manager();
     for (Input_Manager::Axis& axis : input_manager.axes) {
         if (axis_name == axis.axis) {
             return axis.value;
         }
     }
-    return 0;
+    return 0;*/
+    return get_axis_raw(axis_name);
 }
 
 float input::get_axis_raw(Input_Axis axis_name) {
