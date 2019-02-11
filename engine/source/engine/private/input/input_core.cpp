bd19c4ec5545c17fda018eea1c6a79700d4ace59
Piotr Kocia <p.kocia@wp.pl>
2019-02-11 13:19:13 +0100

    Changed get_axis to use get_axis_raw because no implementation yet

--- engine/source/engine/private/input/input_core.cpp
+++ engine/source/engine/private/input/input_core.cpp
@@ -7,132 +7,7 @@
 #include <unordered_map>
 
 static void extract_bindings(std::string const& str, std::vector<Input_Axis_Binding>& axis_bindings, std::vector<Input_Action_Binding>& action_bindings) {
-    // clang-format off
-    std::unordered_map<std::string, Key> string_to_key({
-        {"any_key", Key::any_key},
-        {"mouse_x", Key::mouse_x},
-        {"mouse_y", Key::mouse_y},
-        {"mouse_scroll", Key::mouse_scroll},
-        {"mouse_scroll_up", Key::mouse_scroll_up},
-        {"mouse_scroll_down", Key::mouse_scroll_down},
-        {"mouse_0", Key::mouse_0},
-        {"mouse_1", Key::mouse_1},
-        {"mouse_2", Key::mouse_2},
-        {"mouse_3", Key::mouse_3},
-        {"mouse_4", Key::mouse_4},
-        {"mouse_5", Key::mouse_5},
-        {"mouse_6", Key::mouse_6},
-        {"mouse_7", Key::mouse_7},
-        {"escape", Key::escape},
-        {"enter", Key::enter},
-        {"tab", Key::tab},
-        {"caps_lock", Key::caps_lock},
-        {"spacebar", Key::spacebar},
-        {"backspace", Key::backspace},
-        {"keypad_0", Key::keypad_0},
-        {"keypad_1", Key::keypad_1},
-        {"keypad_2", Key::keypad_2},
-        {"keypad_3", Key::keypad_3},
-        {"keypad_4", Key::keypad_4},
-        {"keypad_5", Key::keypad_5},
-        {"keypad_6", Key::keypad_6},
-        {"keypad_7", Key::keypad_7},
-        {"keypad_8", Key::keypad_8},
-        {"keypad_9", Key::keypad_9},
-        {"keypad_slash", Key::keypad_slash},
-        {"keypad_period", Key::keypad_period},
-        {"keypad_asteriks", Key::keypad_asteriks},
-        {"keypad_minus", Key::keypad_minus},
-        {"keypad_plus", Key::keypad_plus},
-        {"keypad_enter", Key::keypad_enter},
-        {"left", Key::left},
-        {"right", Key::right},
-        {"up", Key::up},
-        {"down", Key::down},
-        {"insert", Key::insert},
-        {"home", Key::home},
-        {"page_up", Key::page_up},
-        {"page_down", Key::page_down},
-        {"end", Key::end},
-        {"delete_key", Key::delete_key},
-        {"one", Key::one},
-        {"two", Key::two},
-        {"three", Key::three},
-        {"four", Key::four},
-        {"five", Key::five},
-        {"six", Key::six},
-        {"seven", Key::seven},
-        {"eight", Key::eight},
-        {"nine", Key::nine},
-        {"zero", Key::zero},
-        {"minus", Key::minus},
-        {"plus", Key::plus},
-        {"tilde", Key::tilde},
-        {"a", Key::a},
-        {"b", Key::b},
-        {"c", Key::c},
-        {"d", Key::d},
-        {"e", Key::e},
-        {"f", Key::f},
-        {"g", Key::g},
-        {"h", Key::h},
-        {"i", Key::i},
-        {"j", Key::j},
-        {"k", Key::k},
-        {"l", Key::l},
-        {"m", Key::m},
-        {"n", Key::n},
-        {"o", Key::o},
-        {"p", Key::p},
-        {"q", Key::q},
-        {"r", Key::r},
-        {"s", Key::s},
-        {"t", Key::t},
-        {"u", Key::u},
-        {"v", Key::v},
-        {"w", Key::w},
-        {"x", Key::x},
-        {"y", Key::y},
-        {"z", Key::z},
-        {"left_curly_bracket", Key::left_curly_bracket},
-        {"right_curly_bracket", Key::right_curly_bracket},
-        {"pipe", Key::pipe},
-        {"colon", Key::colon},
-        {"semicolon", Key::semicolon},
-        {"f1", Key::f1},
-        {"f2", Key::f2},
-        {"f3", Key::f3},
-        {"f4", Key::f4},
-        {"f5", Key::f5},
-        {"f6", Key::f6},
-        {"f7", Key::f7},
-        {"f8", Key::f8},
-        {"f9", Key::f9},
-        {"f10", Key::f10},
-        {"f11", Key::f11},
-        {"f12", Key::f12},
-        {"gamepad_button_0", Key::gamepad_button_0},
-        {"gamepad_button_1", Key::gamepad_button_1},
-        {"gamepad_button_2", Key::gamepad_button_2},
-        {"gamepad_button_3", Key::gamepad_button_3},
-        {"gamepad_button_4", Key::gamepad_button_4},
-        {"gamepad_button_5", Key::gamepad_button_5},
-        {"gamepad_button_6", Key::gamepad_button_6},
-        {"gamepad_button_7", Key::gamepad_button_7},
-        {"gamepad_button_8", Key::gamepad_button_8},
-        {"gamepad_button_9", Key::gamepad_button_9},
-        {"gamepad_button_10", Key::gamepad_button_10},
-        {"gamepad_button_11", Key::gamepad_button_11},
-        {"gamepad_button_12", Key::gamepad_button_12},
-        {"gamepad_button_13", Key::gamepad_button_13},
-        {"gamepad_button_14", Key::gamepad_button_14},
-        {"gamepad_button_15", Key::gamepad_button_15},
-        {"gamepad_button_16", Key::gamepad_button_16},
-        {"gamepad_button_17", Key::gamepad_button_17},
-        {"gamepad_button_18", Key::gamepad_button_18},
-        {"gamepad_button_19", Key::gamepad_button_19}
-    });
-
+	// clang-format off
     std::unordered_map<std::string, Input_Axis> string_to_input_axis({
         {"move_forward", Input_Axis::move_forward},
         {"move_sideways", Input_Axis::move_sideways},
@@ -249,22 +124,22 @@ static void extract_bindings(std::string const& str, std::vector<Input_Axis_Bind
     parse_input_string(str, axes, actions);
 
     for (Binding& axis : axes) {
-        auto key_iter = string_to_key.find(axis.key);
+        auto key = key_from_string(axis.key);
         auto name_iter = string_to_input_axis.find(axis.name);
-        GE_assert(key_iter != string_to_key.end() && name_iter != string_to_input_axis.end(),
+        GE_assert(name_iter != string_to_input_axis.end(),
                   "Attepmting to load unknown axis binding, skipping"); // TODO replace with logging
-        if (key_iter != string_to_key.end() && name_iter != string_to_input_axis.end()) {
-            axis_bindings.emplace_back(name_iter->second, key_iter->second);
+        if (name_iter != string_to_input_axis.end()) {
+            axis_bindings.emplace_back(name_iter->second, key);
         }
     }
 
     for (Binding& action : actions) {
-        auto key_iter = string_to_key.find(action.key);
+        auto key = key_from_string(action.key);
         auto name_iter = string_to_input_action.find(action.name);
-        GE_assert(key_iter != string_to_key.end() && name_iter != string_to_input_action.end(),
+        GE_assert(name_iter != string_to_input_action.end(),
                   "Attepmting to load unknown axis binding, skipping"); // TODO replace with logging
-        if (key_iter != string_to_key.end() && name_iter != string_to_input_action.end()) {
-            action_bindings.emplace_back(name_iter->second, key_iter->second);
+        if (name_iter != string_to_input_action.end()) {
+            action_bindings.emplace_back(name_iter->second, key);
         }
     }
 }
@@ -322,8 +197,11 @@ void Input_Manager::register_action_bindings(std::vector<Input_Action_Binding> c
     }
 }
 
+#include <iostream>
+
 void Input_Manager::process_events() {
     for (Input_Event event : input_event_queue) {
+        std::cout << key_to_string(event.key) << ": " << event.value << "\n";
         for (auto& binding : axis_bindings) {
             if (binding.key == event.key) {
                 for (Axis& axis : axes) {
