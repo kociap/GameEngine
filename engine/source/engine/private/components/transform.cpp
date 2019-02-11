2fa2ab0b7f3cda5d7fbc6bcbd3ab0a6a70991a91
Piotr Kocia <p.kocia@wp.pl>
2019-02-11 13:10:17 +0100

    Implemented transform functions

--- engine/source/engine/private/components/transform.cpp
+++ engine/source/engine/private/components/transform.cpp
@@ -1,15 +1,23 @@
 #include "components/transform.hpp"
 #include "debug_macros.hpp"
+#include "math/transform.hpp"
 
 #include <cmath>
 
+void Transform::translate(Vector3 const& translation_vec) {
+    local_position += translation_vec;
+}
+
+void Transform::scale(Vector3 const& scale_vec) {
+    local_scale.multiply_componentwise(scale_vec);
+}
+
 void Transform::rotate(Vector3 axis, float angle) {
     float half_angle_sin = std::sinf(angle / 2);
-    rotation = Quaternion(axis.x * half_angle_sin, axis.y * half_angle_sin, axis.z * half_angle_sin, std::cosf(angle / 2)) * rotation;
+    local_rotation = Quaternion(axis.x * half_angle_sin, axis.y * half_angle_sin, axis.z * half_angle_sin, std::cosf(angle / 2)) * local_rotation;
 }
 
 Matrix4 Transform::to_matrix() const {
-    GE_assert(false, "Transform::to_matrix is ot implemented");
-    // TODO implement
-    return Matrix4::identity;
+    Matrix4 mat = transform::scale(local_scale) * transform::rotate(local_rotation) * transform::translate(local_position);
+    return mat;
 }
