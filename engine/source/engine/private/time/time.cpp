75d069dd0bdcf3e3d5699ea974178e90e224c72f
Piotr Kocia <p.kocia@wp.pl>
2019-02-11 13:17:25 +0100

    Fixed extern in time implementation

--- engine/source/engine/private/time/time.cpp
+++ engine/source/engine/private/time/time.cpp
@@ -1,24 +1,23 @@
 #include "time.hpp"
 #include "time_core.hpp"
-
-extern Time_Core g_time;
+#include "engine.hpp"
 
 float time::get_delta_time() {
-    return g_time.delta_time;
+    return Engine::get_time_manager().delta_time;
 }
 
 float time::get_time() {
-    return g_time.frame_time;
+    return Engine::get_time_manager().frame_time;
 }
 
 float time::get_unscaled_delta_time() {
-    return g_time.unscaled_delta_time;
+    return Engine::get_time_manager().unscaled_delta_time;
 }
 
 float time::get_unscaled_time() {
-    return g_time.unscaled_frame_time;
+    return Engine::get_time_manager().unscaled_frame_time;
 }
 
 uint64_t time::get_frame_count() {
-    return g_time.frame_count;
+    return Engine::get_time_manager().frame_count;
 }
--- engine/source/engine/public/time.hpp
+++ engine/source/engine/public/time.hpp
@@ -5,15 +5,19 @@
 
 namespace time {
     // The time it took to complete the previous frame
-    static float get_delta_time();
+    float get_delta_time();
+
     // Time scale independent time it took to complete the previous frame
-    static float get_unscaled_delta_time();
+    float get_unscaled_delta_time();
+
     // Time since the start of the game
-    static float get_time();
+    float get_time();
+
     // Time scale independent time since the start of the game
-    static float get_unscaled_time();
+    float get_unscaled_time();
+
     // The number of frames since the start of the game
-    static uint64_t get_frame_count();
+    uint64_t get_frame_count();
 }; // namespace time
 
 #endif // !ENGINE_TIME_HPP_INCLUDE
