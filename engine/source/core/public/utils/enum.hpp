6ab4c554e3af5729d43be403f5ab378fc2e1742b
Piotr Kocia <p.kocia@wp.pl>
2019-05-09 14:40:26 +0200

    fixing enum utils

--- engine/source/core/public/utils/enum.hpp
+++ engine/source/core/public/utils/enum.hpp
@@ -12,31 +12,32 @@ namespace utils {
     template <typename T>
     struct enable_enum_add_operator {};
 
-    template <typename T, typename = std::enable_if_t<enable_enum_add_operator<T>::value>>
-    constexpr T operator+(T a, std::underlying_type_t<T> b) {
-        return static_cast<T>(enum_to_value(a) + b);
-    }
-
-    template <typename T, typename = std::enable_if_t<enable_enum_add_operator<T>::value>>
-    constexpr T operator+(std::underlying_type_t<T> a, T b) {
-        return static_cast<T>(a + enum_to_value(b));
-    }
-
     template <typename T>
     struct enable_enum_bitwise_and {};
-
-    template <typename T, typename = std::enable_if_t<enable_enum_bitwise_and<T>::value>>
-    constexpr T operator&(T a, T b) {
-        return static_cast<T>(enum_to_value(a) & enum_to_value(b));
-    }
 
     template <typename T>
     struct enable_enum_bitwise_or {};
 
-    template <typename T, typename = std::enable_if_t<enable_enum_bitwise_or<T>::value>>
-    constexpr T operator|(T a, T b) {
-        return static_cast<T>(enum_to_value(a) | enum_to_value(b));
-    }
 } // namespace utils
+
+template <typename T, bool = utils::enable_enum_add_operator<T>::value>
+constexpr T operator+(T a, std::underlying_type_t<T> b) {
+    return static_cast<T>(utils::enum_to_value(a) + b);
+}
+
+template <typename T, bool = utils::enable_enum_add_operator<T>::value>
+constexpr T operator+(std::underlying_type_t<T> a, T b) {
+    return static_cast<T>(a + utils::enum_to_value(b));
+}
+
+template <typename T, bool = utils::enable_enum_bitwise_and<T>::value>
+constexpr T operator&(T a, T b) {
+    return static_cast<T>(utils::enum_to_value(a) & utils::enum_to_value(b));
+}
+
+template <typename T, bool = utils::enable_enum_bitwise_or<T>::value>
+constexpr T operator|(T const a, T const b) {
+    return static_cast<T>(utils::enum_to_value(a) | utils::enum_to_value(b));
+}
 
 #endif // !CORE_UTILS_ENUM_HPP_INCLUDE
