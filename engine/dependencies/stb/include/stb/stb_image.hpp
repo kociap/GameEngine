a69589d3ad03ee4884308888cd032ee6145d038b
Piotr Kocia <p.kocia@wp.pl>
2019-09-11 20:23:52 +0200

    Removed stb from dependencies

--- engine/dependencies/stb/include/stb/stb_image.hpp
+++
@@ -1,6 +0,0 @@
-//
-//	Header wrapper for stb_image
-//
-
-#include "../source/stb_image.h" // I don't know why, but msvc cannot find this header if added as PRIVATE in CMake
-                                 //   so I added it as relative
--- engine/dependencies/stb/source/stb_image.c
+++
@@ -1,6 +0,0 @@
-//
-//	Implementation of stb
-//
-
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
