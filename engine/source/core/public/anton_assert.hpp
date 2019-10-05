e86e94ec993662fe052168e59282fab1858a767f
Piotr Kocia <p.kocia@wp.pl>
2019-10-05 23:36:32 +0200

    intrinsics in assert

--- engine/source/core/public/anton_assert.hpp
+++ engine/source/core/public/anton_assert.hpp
@@ -2,12 +2,13 @@
 #define CORE_ASSERT_HPP_INCLUDE
 
 #include <build_config.hpp>
+#include <intrinsics.hpp>
 
 namespace anton_engine {
     void anton_assert(char const* message, char const* file, unsigned long long line);
 
 #if ANTON_DEBUG
-#    define ANTON_ASSERT(condition, msg) (static_cast<bool>(condition) ? (void)0 : anton_engine::anton_assert(msg, __FILE__, __LINE__))
+#    define ANTON_ASSERT(condition, msg) (ANTON_LIKELY(static_cast<bool>(condition)) ? (void)0 : anton_engine::anton_assert(msg, __FILE__, __LINE__))
 #else
 #    define ANTON_ASSERT(condition, msg) ((void)0)
 #endif
@@ -19,7 +20,7 @@ namespace anton_engine {
 #if !ANTON_DISABLE_VERIFY
 // ANTON_VERIFY
 // Debug and Release builds assert. Must be disabled explicitly.
-#    define ANTON_VERIFY(condition, msg) (static_cast<bool>(condition) ? (void)0 : anton_engine::anton_assert(msg, __FILE__, __LINE__))
+#    define ANTON_VERIFY(condition, msg) (ANTON_LIKELY(static_cast<bool>(condition)) ? (void)0 : anton_engine::anton_assert(msg, __FILE__, __LINE__))
 #else
 #    define ANTON_VERIFY() ((void)0)
 #endif // !ANTON_DISABLE_VERIFY
