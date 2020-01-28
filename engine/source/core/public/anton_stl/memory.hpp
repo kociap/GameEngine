f2c2ecac721cc61fc9a62dc2c3c999d313c7afd9
Piotr Kocia <p.kocia@wp.pl>
2020-01-28 21:09:01 +0100

    implemented Vector::insert

--- engine/source/core/public/anton_stl/memory.hpp
+++ engine/source/core/public/anton_stl/memory.hpp
@@ -181,10 +181,54 @@ namespace anton_engine::anton_stl {
                                    anton_stl::Is_Iterator_Category<output_iterator_cat, anton_stl::Contiguous_Iterator_Tag>>;
         if constexpr (both_iterators_are_contiguous && anton_stl::is_trivial<input_value_type> && anton_stl::is_same<input_value_type, output_value_type>) {
             // TODO: Assumes that contiguous iterators are pointers.
-            return static_cast<Output_Iterator>(memmove(dest, first, (last - first) * sizeof(input_value_type)));
+            memmove(dest, first, (usize)(last - first) * sizeof(input_value_type));
+            return dest + (last - first);
         } else {
             for (; first != last; ++first, ++dest) {
                 *dest = *first;
+            }
+            return dest;
+        }
+    }
+
+    // copy_backward
+    //
+    // Copy assigns elements from range [first, last[ to dest in reverse order (last element is copied first) decrementing dest after each copy.
+    // The back of the range [first, last[ may overlap the front of the range [dest - (first - last), dest[.
+    //
+    // Returns iterator to the beginning of the destination range.
+    //
+    // Example usage:
+    //   anton_stl::copy_backward(array.begin(), array.end(), dest_array.end());
+    //
+    // Reference implementation:
+    //   template<typename Bidirectional_Iterator>
+    //   Bidirectional_Iterator copy_backward(Bidirectional_Iterator first, Bidirectional_Iterator last, Bidirectional_Iterator dest) {
+    //       while (last != first) {
+    //           *--dest = *--last;
+    //       }
+    //       return dest;
+    //   }
+    //
+    template <typename Bidirectional_Iterator, typename Dest_Bidirectional_Iterator>
+    inline Dest_Bidirectional_Iterator copy_backward(Bidirectional_Iterator first, Bidirectional_Iterator last, Dest_Bidirectional_Iterator dest) {
+        // TODO: Iterator unwrapping
+        using input_iterator_cat = typename anton_stl::Iterator_Traits<Bidirectional_Iterator>::iterator_category;
+        using output_iterator_cat = typename anton_stl::Iterator_Traits<Dest_Bidirectional_Iterator>::iterator_category;
+        using input_value_type = typename anton_stl::Iterator_Traits<Bidirectional_Iterator>::value_type;
+        using output_value_type = typename anton_stl::Iterator_Traits<Dest_Bidirectional_Iterator>::value_type;
+
+        constexpr bool both_iterators_are_contiguous =
+            anton_stl::conjunction<anton_stl::Is_Iterator_Category<input_iterator_cat, anton_stl::Contiguous_Iterator_Tag>,
+                                   anton_stl::Is_Iterator_Category<output_iterator_cat, anton_stl::Contiguous_Iterator_Tag>>;
+        if constexpr (both_iterators_are_contiguous && anton_stl::is_trivial<input_value_type> && anton_stl::is_same<input_value_type, output_value_type>) {
+            // TODO: Assumes that contiguous iterators are pointers.
+            Dest_Bidirectional_Iterator const dest_begin = dest - (last - first);
+            memmove(dest_begin, first, (usize)(last - first) * sizeof(input_value_type));
+            return dest_begin;
+        } else {
+            while (last != first) {
+                *--dest = *--last;
             }
             return dest;
         }
@@ -207,10 +251,54 @@ namespace anton_engine::anton_stl {
                                                                               anton_stl::Is_Same<output_iterator_cat, anton_stl::Contiguous_Iterator_Tag>>;
         if constexpr (both_iterators_are_contiguous && anton_stl::is_trivial<input_value_type> && anton_stl::is_same<input_value_type, output_value_type>) {
             // TODO: Assumes that contiguous iterators are pointers.
-            return static_cast<Output_Iterator>(memmove(dest, first, (last - first) * sizeof(input_value_type)));
+            memmove(dest, first, (usize)(last - first) * sizeof(input_value_type));
+            return dest + (last - first);
         } else {
             for (; first != last; ++first, ++dest) {
                 *dest = anton_stl::move(*first);
+            }
+            return dest;
+        }
+    }
+
+    // move_backward
+    //
+    // Move assigns elements from range [first, last[ to dest in reverse order (last element is copied first) decrementing dest after each move.
+    // The back of the range [first, last[ may overlap the front of the range [dest - (first - last), dest[.
+    //
+    // Returns iterator to the beginning of the destination range.
+    //
+    // Example usage:
+    //   anton_stl::move_backward(array.begin(), array.end(), dest_array.end());
+    //
+    // Reference implementation:
+    //   template<typename Bidirectional_Iterator>
+    //   Bidirectional_Iterator move_backward(Bidirectional_Iterator first, Bidirectional_Iterator last, Bidirectional_Iterator dest) {
+    //       while (last != first) {
+    //           *--dest = anton_stl::move(*--last);
+    //       }
+    //       return dest;
+    //   }
+    //
+    template <typename Bidirectional_Iterator, typename Dest_Bidirectional_Iterator>
+    inline Dest_Bidirectional_Iterator move_backward(Bidirectional_Iterator first, Bidirectional_Iterator last, Dest_Bidirectional_Iterator dest) {
+        // TODO: Iterator unwrapping
+        using input_iterator_cat = typename anton_stl::Iterator_Traits<Bidirectional_Iterator>::iterator_category;
+        using output_iterator_cat = typename anton_stl::Iterator_Traits<Dest_Bidirectional_Iterator>::iterator_category;
+        using input_value_type = typename anton_stl::Iterator_Traits<Bidirectional_Iterator>::value_type;
+        using output_value_type = typename anton_stl::Iterator_Traits<Dest_Bidirectional_Iterator>::value_type;
+
+        constexpr bool both_iterators_are_contiguous =
+            anton_stl::conjunction<anton_stl::Is_Iterator_Category<input_iterator_cat, anton_stl::Contiguous_Iterator_Tag>,
+                                   anton_stl::Is_Iterator_Category<output_iterator_cat, anton_stl::Contiguous_Iterator_Tag>>;
+        if constexpr (both_iterators_are_contiguous && anton_stl::is_trivial<input_value_type> && anton_stl::is_same<input_value_type, output_value_type>) {
+            // TODO: Assumes that contiguous iterators are pointers.
+            Dest_Bidirectional_Iterator const dest_begin = dest - (last - first);
+            memmove(dest_begin, first, (usize)(last - first) * sizeof(input_value_type));
+            return dest_begin;
+        } else {
+            while (last != first) {
+                *--dest = *--last;
             }
             return dest;
         }
