c72aeae2f26acdd50f0a03c1ab100e8a59a60a4c
Piotr Kocia <p.kocia@wp.pl>
2020-03-09 21:17:32 +0100

    string rasterizing

---
+++ engine/source/public/rendering/fonts.hpp
@@ -0,0 +1,49 @@
+#ifndef RENDERING_FONTS_HPP_INCLUDE
+#define RENDERING_FONTS_HPP_INCLUDE
+
+#include <core/stl/slice.hpp>
+#include <core/types.hpp>
+#include <core/stl/string_view.hpp>
+#include <core/stl/vector.hpp>
+
+namespace anton_engine::rendering {
+    class Font_Face;
+
+    class Font_Render_Info {
+    public:
+        u32 points;
+        u32 h_dpi;
+        u32 v_dpi;
+    };
+
+    // All metrics are expressed in 26.6 pixel format (1/64 of a pixel).
+    //
+    class Glyph {
+    public:
+        i32 width;
+        i32 height;
+
+        i32 horiBearingX;
+        i32 horiBearingY;
+        i32 horiAdvance;
+
+        i32 vertBearingX;
+        i32 vertBearingY;
+        i32 vertAdvance;
+
+        u32 bitmap_row_width; // in bytes
+        anton_stl::Vector<u8> bitmap;
+    };
+
+    void init_font_rendering();
+    void terminate_font_rendering();
+
+    // Returns:
+    // Strongly typed handle that uniquely identifies the face or nullptr if failed to load the face.
+    //
+    Font_Face* load_face(anton_stl::Slice<u8 const> file_data, u32 face_index);
+    void unload_face(Font_Face* face);
+    anton_stl::Vector<Glyph> render_face(Font_Face* face, Font_Render_Info info, anton_stl::String_View string);
+}
+
+#endif // !RENDERING_FONTS_HPP_INCLUDE
